fe582b921bb5ccf5e8449afbf2447f97af8668bb
Christopher Kalfas <chriskalfas@yahoo.com>
2019-02-05 20:51:47 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -13,6 +13,9 @@ def input_to_index(user_input)
 end
 
 def move (board, index, value)
-  
+  board = [" ", " ", " "]
+def update_array_at_with(array, index, value)
+  array[index] = value
+end
   
 end
